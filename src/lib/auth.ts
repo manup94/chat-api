fef42a0b2a261638234614acac64467478e3fac3
ManuelPerezPrado <mperezpradosjose@gmail.com>
2026-04-25 00:22:07 +0200

    try to fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { Request } from "express"
 
-export const AUTH_COOKIE_NAME = "next-auth.session-token"
+export const AUTH_COOKIE_NAME = "__Secure-next-auth.session-token"
 export const API_INTERNAL_HEADER = "x-internal-api-secret"
 
 const isProduction = process.env.NODE_ENV === "production"
